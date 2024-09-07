3af556eb6aaa98acd53334e7daeb14cde9c7d949
Mohammad Athar Raja Faridi <atharfaridi@outlook.com>
2024-09-07 11:59:11 +0100

    Added Favicon Icons to the site

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,17 +3,21 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import LogoWithThumbnail  from "./components/LogoWithThumbnail";
 
 const config: DocsThemeConfig = {
-  logo: <LogoWithThumbnail/>,
-   head: (
+  logo: <LogoWithThumbnail />,
+  head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Athar Faridi" />
       <meta property="og:description" content="Welcome to my playground !" />
+      <title>Athar Faridi</title>
+      <link rel="icon" href="https://trekohike.com/wp-content/uploads/2024/05/cropped-Trekohike-by-Saga-Photoroom-1-e1717754370583-32x32.webp" sizes="32x32"></link>
     </>
   ),
   footer: {
-    text: '2016-2024 ©atharfaridi',
+    text: "2016-2024 ©atharfaridi",
   },
-}
+  docsRepositoryBase:"https://github.com/atharefaridi/tech-blog/tree/main",
+  
+};
 
 export default config
