5fb48f8c8e331ddcca2141d388e8144b28dcc5e5
Mohammad Athar Raja Faridi <atharfaridi@outlook.com>
2024-09-07 11:36:32 +0100

    Book a Call

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,13 @@ import LogoWithThumbnail  from "./components/LogoWithThumbnail";
 
 const config: DocsThemeConfig = {
   logo: <LogoWithThumbnail/>,
-
+   head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Athar Faridi" />
+      <meta property="og:description" content="Welcome to my playground !" />
+    </>
+  ),
   footer: {
     text: '2016-2024 ©atharfaridi',
   },
